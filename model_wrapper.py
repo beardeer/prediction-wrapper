f4fe0636dc9f9a22b9ed58e7b8248a2de37fb6e5
Xiaolu Xiong <xiaolu@Xiaolus-MBP.home>
2016-11-06 13:41:31 -0500

    fix structure

---
+++ model_wrapper.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+class ModelWrapper(object):
+
+	__metaclass__ = ABCMeta
+
+	def __init__(self, data_frame, feature_names, label_name, categorical_feature_names = None):
+		self.data_frame = data_frame
+		self.feature_names = feature_names
+		self.label_name = label_name
+		self.categorical_feature_names = categorical_feature_names
+		self.model = None
+
+	def set_up_model(self, model):
+		self.model = model
+
+	@abstractmethod
+	def run(self):
+
+		raise NotImplementedError()
+
+
+		
+
