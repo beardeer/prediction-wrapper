492cf53306a160605dee35b5491b6a486a003ac0
Xiaolu Xiong <xiaolu@Xiaolus-MBP.home>
2016-11-06 13:47:06 -0500

    fix space

--- binary_classifier_wrapper.py
+++ binary_classifier_wrapper.py
@@ -7,63 +7,59 @@ from model_wrapper import ModelWrapper
 
 class KfoldBinaryClassifierWrapper(ModelWrapper):
 
-	def __init__(self, data_frame, feature_names, label_name, categorical_feature_names = None, k = 5):
-		ModelWrapper.__init__(self, data_frame, feature_names, label_name, categorical_feature_names)
+    def __init__(self, data_frame, feature_names, label_name, categorical_feature_names = None, k = 5):
+        ModelWrapper.__init__(self, data_frame, feature_names, label_name, categorical_feature_names)
 
-		self.cv = None
-		self.k = k
-		self._generate_split_index()
+        self.cv = None
+        self.k = k
+        self._generate_split_index()
 
-	def _generate_split_index(self):
-		self.cv = KFold(n_splits = self.k, shuffle = True)
+    def _generate_split_index(self):
+        self.cv = KFold(n_splits = self.k, shuffle = True)
 
-	def _split_data(self, train_idx, test_idx):
-		x_train = (self.data_frame[self.feature_names].iloc[train_idx,:])
-		y_train = self.data_frame[self.label_name].iloc[train_idx]
-		x_test = (self.data_frame[self.feature_names].iloc[test_idx,:])
-		y_test = self.data_frame[self.label_name].iloc[test_idx]
+    def _split_data(self, train_idx, test_idx):
+        x_train = (self.data_frame[self.feature_names].iloc[train_idx,:])
+        y_train = self.data_frame[self.label_name].iloc[train_idx]
+        x_test = (self.data_frame[self.feature_names].iloc[test_idx,:])
+        y_test = self.data_frame[self.label_name].iloc[test_idx]
+        return x_train, y_train, x_test, y_test
 
-		return x_train, y_train, x_test, y_test
+    def _transform_categorical_featurs(self):
+        le = LabelEncoder()
+        for name in self.categorical_feature_names:
+            self.data_frame[name] = le.fit_transform(self.data_frame[name])
 
-	def _transform_categorical_featurs(self):
+    def _onehot_categorical_featurs(self, train_data, test_data):
+        if self.categorical_feature_names == None:
+            return train_data, test_data
 
-		le = LabelEncoder()
-		for name in self.categorical_feature_names:
-			self.data_frame[name] = le.fit_transform(self.data_frame[name])
+        feature_idxs = [self.data_frame.columns.get_loc(name) for name in self.categorical_feature_names]
+        encoder = OneHotEncoder(categorical_features = feature_idxs)
+        encoder.fit(np.vstack((train_data, test_data)))
+        train_data = encoder.transform(train_data)
+        test_data = encoder.transform(test_data)
 
-	def _onehot_categorical_featurs(self, train_data, test_data):
+        return train_data, test_data
 
-		if self.categorical_feature_names == None:
-			return train_data, test_data
+    def run(self):
 
-		feature_idxs = [self.data_frame.columns.get_loc(name) for name in self.categorical_feature_names]
-		encoder = OneHotEncoder(categorical_features = feature_idxs)
-		encoder.fit(np.vstack((train_data, test_data)))
-		train_data = encoder.transform(train_data)
-		test_data = encoder.transform(test_data)
-		
-		return train_data, test_data
+        results = pd.DataFrame(index = [i for i in range(len(self.data_frame))], columns = ['label', 'pred_prob', 'pred_label'])
+        self._transform_categorical_featurs()
 
-	def run(self):
+        for train_idx, test_idx in self.cv.split(self.data_frame):
+            x_train, y_train, x_test, y_test = self._split_data(train_idx, test_idx)
+            x_train, x_test = self._onehot_categorical_featurs(x_train, x_test)
 
-		results = pd.DataFrame(index = [i for i in range(len(self.data_frame))], columns = ['label', 'pred_prob', 'pred_label'])
+            self.model.fit(x_train, y_train)
 
-		self._transform_categorical_featurs()
+            y_pred_p = self.model.predict_proba(x_test)[:, 1]
+            y_pred_l = self.model.predict(x_test)
 
-		for train_idx, test_idx in self.cv.split(self.data_frame):
-			x_train, y_train, x_test, y_test = self._split_data(train_idx, test_idx)
-			x_train, x_test = self._onehot_categorical_featurs(x_train, x_test)
+            results['label'].iloc[test_idx] = y_test
+            results['pred_prob'].iloc[test_idx] = y_pred_p
+            results['pred_label'].iloc[test_idx] = y_pred_l
 
-        	self.model.fit(x_train, y_train)
-
-        	y_pred_p = self.model.predict_proba(x_test)[:, 1]
-        	y_pred_l = self.model.predict(x_test)
-
-        	results['label'].iloc[test_idx] = y_test
-        	results['pred_prob'].iloc[test_idx] = y_pred_p
-        	results['pred_label'].iloc[test_idx] = y_pred_l
-		
-		return results
+        return results
 
 
 
--- model_wrapper.py
+++ model_wrapper.py
@@ -2,23 +2,21 @@ from abc import ABCMeta, abstractmethod
 
 class ModelWrapper(object):
 
-	__metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-	def __init__(self, data_frame, feature_names, label_name, categorical_feature_names = None):
-		self.data_frame = data_frame
-		self.feature_names = feature_names
-		self.label_name = label_name
-		self.categorical_feature_names = categorical_feature_names
-		self.model = None
+    def __init__(self, data_frame, feature_names, label_name, categorical_feature_names = None):
+        self.data_frame = data_frame
+        self.feature_names = feature_names
+        self.label_name = label_name
+        self.categorical_feature_names = categorical_feature_names
+        self.model = None
 
-	def set_up_model(self, model):
-		self.model = model
+    def set_up_model(self, model):
+        self.model = model
 
-	@abstractmethod
-	def run(self):
+    @abstractmethod
+    def run(self):
 
-		raise NotImplementedError()
-
-
-		
+        raise NotImplementedError()
+        
 
