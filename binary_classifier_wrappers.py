9eb55d2dce2d882ee017c942da676955432954d3
Xiaolu Xiong <beardeer@gmail.com>
2016-11-07 00:09:05 -0500

    update doc strings

--- binary_classifier_wrappers.py
+++ binary_classifier_wrappers.py
@@ -1,5 +1,8 @@
-"""Summary
+"""The implementation of classifier wrappers
 """
+
+# Author: Xiaolu Xiong <beardeer@gmail.com>
+
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import numpy as np
@@ -9,61 +12,46 @@ from .model_wrapper import ModelWrapper
 from .pred_results import BinaryPredResult
 
 class KfoldBinaryClassifierWrapper(ModelWrapper):
-    """Summary
+    """The class runs k-fold cross-validation on a sklearn classifier model.
 
     Attributes
     ----------
-    k : TYPE
-        Description
-    kfold : TYPE
-        Description
+    k : int
+        The number of k folds in cross-validation
+    kfold : sklearn KFold
+        K-Folds cross-validator for a given k
     """
-    def __init__(self, data_frame, label_name, feature_names, categorical_feature_names = [], k = 5):
-        """Summary
-
-        Parameters
-        ----------
-        data_frame : TYPE
-            Description
-        label_name : TYPE
-            Description
-        feature_names : TYPE
-            Description
-        categorical_feature_names : list, optional
-            Description
-        k : int, optional
-            Description
-        """
+    def __init__(self, data_frame, label_name, feature_names, 
+    	categorical_feature_names = [], k = 5):
         ModelWrapper.__init__(self, data_frame, label_name, feature_names, categorical_feature_names)
 
         self.k = k
-        self.kfold = None
-        self._generate_split_index()
+        self._kfold = None
+        self._generate_kfold()
 
-    def _generate_split_index(self):
-        """Summary
+    def _generate_kfold(self):
+        """Generate a K-Folds cross-validator for a given k.
 
         Returns
         -------
-        TYPE
-            Description
+        None
         """
-        self.kfold = KFold(n_splits = self.k, shuffle = True)
+        self._kfold = KFold(n_splits = self.k, shuffle = True)
 
     def _split_data(self, train_idx, test_idx):
-        """Summary
+        """Split the data_frame by training index and testing index. 
 
         Parameters
         ----------
-        train_idx : TYPE
-            Description
-        test_idx : TYPE
-            Description
+        train_idx : list
+            A list of traning indexes
+        test_idx : list
+            A list of testing indexes
 
         Returns
         -------
-        TYPE
-            Description
+        DataFrames
+            DataFrames that used in current fold of cross validation
         """
         x_train = (self.data_frame[self.feature_names].iloc[train_idx,:])
         y_train = self.data_frame[self.label_name].iloc[train_idx]
@@ -72,37 +60,47 @@ class KfoldBinaryClassifierWrapper(ModelWrapper):
         return x_train, y_train, x_test, y_test
 
     def _transform_categorical_featurs(self):
-        """Summary
+        """Utilize the sklearn LabelEncoder to encode categorical features.
+
+        This is necessary for using categorical values that are represented in strings. 
 
         Returns
         -------
-        TYPE
-            Description
+        None
         """
         le = LabelEncoder()
+        # Transform each categorical feature with value between 0 and n_classes-1
         for name in self.categorical_feature_names:
             self.data_frame[name] = le.fit_transform(self.data_frame[name])
 
     def _onehot_categorical_featurs(self, train_data, test_data):
-        """Summary
+        """Utilize the sklearn OneHotEncoder to encode categorical features.
+
+        In order to feed categorical features to sklearn model, we need to convert
+        them to one-hot encoding. 
+
+        There are other different ways of using categorical features, such as using
+        pandas.get_dummies, but get_dummies will create additional features in
+        data_frame, which I perfer not to that.  
 
         Parameters
         ----------
-        train_data : TYPE
-            Description
-        test_data : TYPE
-            Description
+        train_data : DataFrame
+            Traning data
+        test_data : DataFrame
+            Testing data
 
         Returns
         -------
-        TYPE
-            Description
+        numpy matrix
+            two numpy matrices with one-hot encoded features
         """
         if self.categorical_feature_names == []:
             return train_data, test_data
-
+        # Select indexs of categorical features, and encode, then transform them
         feature_idxs = [self.data_frame.columns.get_loc(name) for name in self.categorical_feature_names]
         encoder = OneHotEncoder(categorical_features = feature_idxs)
+        # Need to fit on every values from both training and testing
         encoder.fit(np.vstack((train_data, test_data)))
         train_data = encoder.transform(train_data)
         test_data = encoder.transform(test_data)
@@ -110,22 +108,25 @@ class KfoldBinaryClassifierWrapper(ModelWrapper):
         return train_data, test_data
 
     def run(self):
-        """Summary
+        """Run classifier model with k-fold cross-validation
 
         Returns
         -------
-        TYPE
-            Description
+        PredResult
+            Model-generated Prediction result
         """
         result = BinaryPredResult(len(self.data_frame))
         self._transform_categorical_featurs()
 
-        for train_idx, test_idx in self.kfold.split(self.data_frame):
+        # Run k-fold cross-validation
+        for train_idx, test_idx in self._kfold.split(self.data_frame):
             x_train, y_train, x_test, y_test = self._split_data(train_idx, test_idx)
             x_train, x_test = self._onehot_categorical_featurs(x_train, x_test)
 
+            # Training
             self.model.fit(x_train, y_train)
 
+            # Testing and generating predictions
             y_pred_p = self.model.predict_proba(x_test)[:, 1]
             y_pred_l = self.model.predict(x_test)
 
